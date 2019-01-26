8ae3e87482a504902f93ca293deb50b7217899a9
Stephen Wolfe <fridgewolf@protonmail.com>
2019-01-25 19:04:42 -0500

    Newest new

--- testOrdinalize.py
+++ testOrdinalize.py
@@ -1,12 +1,13 @@
 import requests, ordinalize
 
 """
-I wrote this program to check the accuracy of ordinalize.py, also found in this repository.
-It uses requests to go to http://www.isthe.com/cgi-bin/chongo/number.cgi and submit the
-number input by the user to the form there. It then downloads the HTTP response, parses it
-for and removes the number, ordinalizes the number (five -> fifth, etc) and then uses
-ordinalize.py as a module to get what should be the same number. It lastly removes 
-punctuation, checks whether the two strings are equal, and lets the user know either way.
+This program uses Requests to check the accuracy of ordinalize.py, also found in this 
+repository. It uses requests to go to http://www.isthe.com/cgi-bin/chongo/number.cgi 
+and submit the number input by the user to the form there. It then downloads the HTTP 
+response, parses it for and removes the number, ordinalizes the number (five -> fifth, 
+etc) and then uses ordinalize.py as a module to get what should be the same number. 
+It lastly removes punctuation, checks whether the two strings are equal, and lets the 
+user know either way.
 
 For now it will be used to check my progress toward infinity but when I hammer out the kinks
 in ordinalize.py I'll do the same for this guy.
